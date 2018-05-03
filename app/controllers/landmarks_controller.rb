03fe569dcabf69b6d593733755833d414070a1b9
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-03 02:48:53 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -21,6 +21,7 @@ class LandmarksController < ApplicationController
 
   post "/landmarks/:id" do
     @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
     redirect "/landmarks/#{@landmarks.id}"
   end
 
