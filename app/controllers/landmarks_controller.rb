9dc67049fcf2d8056c915d331c9d5b911944e62b
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-03 02:40:31 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,45 +11,21 @@ class LandmarksController < ApplicationController
 
   post "/landmarks" do
     @landmarks = Landmark.create(params["landmark"])
-    redirect "/figures/#{@figure.id}"
+    redirect "/landmarks/#{@landmarks.id}"
   end
 
-  get "/figures/:id/edit" do
-    @figure = Figure.find(params[:id])
-    @titles = Title.all
-    @landmarks = Landmark.all
-    erb :'/figures/edit'
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
   end
 
-  post "/figures/:id" do
-    @figure = Figure.find(params[:id])
-    @figure.update(name: params["figure"]["name"])
-
-    @figure.titles.clear
-    if params["figure"]["title_ids"]
-      params["figure"]["title_ids"].each do |id|
-       @figure.titles << Title.find(id)
-      end
-    end
-  if !params["title"]["name"].empty?
-      @figure.titles << Title.create(name: params["title"]["name"])
-    end
-
-    @figure.landmarks.clear
-    if params["figure"]["landmark_ids"]
-      params["figure"]["landmark_ids"].each do |id|
-       @figure.landmarks << Landmark.find(id)
-      end
-    end
-    if !params["landmark"]["name"].empty?
-      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
-    end
-    @figure.save
-    redirect "/figures/#{@figure.id}"
+  post "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    redirect "/landmarks/#{@landmarks.id}"
   end
 
-  get "/figures/:id" do
-    @figure = Figure.find(params[:id])
-    erb :'/figures/show'
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
   end
 end
